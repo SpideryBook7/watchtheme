35e42486e21ded616f06234d3a00d6feba75f7ff
SpideryBook78 <ch374185@gmail.com>
2025-12-14 11:52:17 -0600

    Fix Web Crash: Add kIsWeb guard to device type check

--- lib/utils/device.dart
+++ lib/utils/device.dart
@@ -1,17 +1,32 @@
+import 'package:flutter/foundation.dart'; // For kIsWeb
 import 'package:device_info_plus/device_info_plus.dart';
 
 Future<String> getDeviceType() async {
-  final deviceInfo = DeviceInfoPlugin();
-  final androidInfo = await deviceInfo.androidInfo;
-
-  final model = androidInfo.model.toLowerCase();
-  final features = androidInfo.systemFeatures;
-
-  if (model.contains('tv') || features.contains('android.software.leanback')) {
-    return 'tv';
-  } else if (model.contains('wear') || features.contains('android.hardware.type.watch')) {
-    return 'wear';
-  } else {
+  // Web does not support androidInfo and will crash.
+  if (kIsWeb) {
     return 'mobile';
   }
+
+  final deviceInfo = DeviceInfoPlugin();
+
+  try {
+    // This throws on iOS/Windows/Linux/MacOS
+    final androidInfo = await deviceInfo.androidInfo;
+
+    final model = androidInfo.model.toLowerCase();
+    final features = androidInfo.systemFeatures;
+
+    if (model.contains('tv') ||
+        features.contains('android.software.leanback')) {
+      return 'tv';
+    } else if (model.contains('wear') ||
+        features.contains('android.hardware.type.watch')) {
+      return 'wear';
+    }
+  } catch (e) {
+    // If we are not on Android (e.g. iOS), default to mobile.
+    return 'mobile';
+  }
+
+  return 'mobile';
 }
